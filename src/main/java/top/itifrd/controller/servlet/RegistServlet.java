54e589305033352313764e12929dcb024a7e55a5
cyl <1257997441@qq.com>
2022-05-15 20:29:18 +0800

    一点点修改，预做持久化登录

--- src/main/java/top/itifrd/controller/servlet/RegistServlet.java
+++ src/main/java/top/itifrd/controller/servlet/RegistServlet.java
@@ -5,7 +5,9 @@ import top.itifrd.filter.utils.FilterUtils;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
 
@@ -17,32 +19,15 @@ import java.io.IOException;
  * @Version 1.0
  **/
 @WebServlet("/regist")
-public class RegistServlet implements Servlet {
+public class RegistServlet extends HttpServlet {
+
     @Override
-    public void init(ServletConfig servletConfig) throws ServletException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
     }
 
     @Override
-    public ServletConfig getServletConfig() {
-        return null;
-    }
-
-    @Override
-    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
-        // 获取上下文内容，用来将来判断上下文中是否包含有注册的信息
-        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-        ServletContext servletContext = httpServletRequest.getServletContext();
-        System.out.println("servlet获取表单数据:" + servletContext.getAttribute("users"));
-    }
-
-    @Override
-    public String getServletInfo() {
-        return null;
-    }
-
-    @Override
-    public void destroy() {
-
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
     }
 }
